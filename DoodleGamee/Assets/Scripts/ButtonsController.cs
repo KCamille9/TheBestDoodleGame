814379220fa89710bd19ef36e394a38131db3c43
KCamille9 <bgi_camille@hotmail.com>
2019-08-14 15:17:30 -0600

    basic player movements added

---
+++ DoodleGamee/Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public enum Directions {Left, Right, Up, Down }
+    //__________________________________________________//
+
+    [SerializeField] PlayerController playerController;
+    [SerializeField] Directions buttonDirection;
+    //__________________________________________________//
+
+    private bool pointerDown;
+
+    private bool isJumping;
+    public float jumpTime;
+    private float jumpTimeCounter;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pointerDown)
+        {
+            if(buttonDirection.Equals(Directions.Right))
+            {
+                playerController.MoveRight();
+            }
+            else if (buttonDirection.Equals(Directions.Left))
+            {
+                playerController.MoveLeft();
+            }
+
+            else if (buttonDirection.Equals(Directions.Up))
+            {
+                /*isJumping = true;
+                jumpTimeCounter = jumpTime;
+                playerController.MoveUp();*/
+                if(playerController.rb.velocity.y == 0)
+                {
+                    playerController.MoveUp();
+                }
+            }
+
+            /*if (buttonDirection.Equals(Directions.Up) && isJumping)
+            {
+                if (jumpTimeCounter > 0)
+                {
+                    playerController.MoveUp();
+                    jumpTimeCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    pointerDown = false;
+                }
+            }*/
+
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+        Debug.Log("Pointer down");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointerDown = false;
+        Debug.Log("Pointer up");
+    }
+
+    public void ReloadScn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ DoodleGamee/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    [SerializeField] Button moveButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+        rb.velocity = new Vector2(2, rb.velocity.y);
+    }
+
+    public void MoveLeft()
+    {
+        rb.velocity = new Vector2(-2, rb.velocity.y);
+    }
+
+    public void MoveUp()
+    {
+        Debug.Log("up btn pressed");
+        //rb.AddForce(Vector2.up * 200);
+        rb.velocity = Vector2.up * 8;
+    }
+
+
+}
